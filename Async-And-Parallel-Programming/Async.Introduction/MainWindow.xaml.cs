cc7864468dc676d84ea8b1d5221593ebccf192ca
InKolev <in.kolev.b@gmail.com>
2016-09-07 16:53:01 +0300

    Added Demo "Async.Introduction"

---
+++ Async-And-Parallel-Programming/Async.Introduction/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Async.Introduction.Models;
+
+namespace Async.Introduction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private IEnumerable<Car> ProcessCarsFile(string filePath)
+        {
+            var cars = new List<Car>(600);
+            var lines = File.ReadAllLines(filePath).Skip(2);
+
+            foreach (var line in lines)
+            {
+                cars.Add(Car.Parse(line));
+            }
+
+            return cars;
+        }
+
+        private void BtnFreezeUI_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(1000);
+
+                this.btnFreezeUI.Content = "UI has been frozen (Click to freeze again)";
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void BtnShowDetails_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedCar = this.listViewCars.SelectedItem ?? "NO CAR SELECTED";
+            MessageBox.Show(selectedCar.ToString(), "Car details", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnLoadCars_Click(object sender, RoutedEventArgs e)
+        {
+            var carsDataPath = @"..\..\..\Data\cars.csv";
+            var cars = this.ProcessCarsFile(carsDataPath);
+
+            this.listViewCars.ItemsSource = cars;
+        }
+    }
+}
---
+++ Async-And-Parallel-Programming/Async.Introduction/Models/Car.cs
@@ -0,0 +1,48 @@
+﻿using Async.Introduction.Infrastructure;
+
+namespace Async.Introduction.Models
+{
+    public class Car
+    {
+        public string Name { get; set; }
+
+        public string Origin { get; set; }
+
+        public double MilesPerGalon { get; set; }
+
+        public double Displacement { get; set; }
+
+        public double Acceleration { get; set; }
+
+        public double Horsepower { get; set; }
+
+        public double Weight { get; set; }
+
+        public int Cylinders { get; set; }
+
+        public int Year { get; set; }
+
+        public static Car Parse(string carData)
+        {
+            var carParams = carData.Split(';');
+
+            return new Car
+            {
+                Name = carParams[0],
+                MilesPerGalon = carParams[1].ToDouble(),
+                Cylinders = carParams[2].ToInteger(),
+                Displacement = carParams[3].ToDouble(),
+                Horsepower = carParams[4].ToDouble(),
+                Weight = carParams[5].ToDouble(),
+                Acceleration = carParams[6].ToDouble(),
+                Year = carParams[7].ToInteger(),
+                Origin = carParams[8]
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Year} - {this.Horsepower} - {this.Weight}";
+        }
+    }
+}
