c6cb28a659248e73caa0fbfb5e91f36ae79fa682
InKolev <in.kolev.b@gmail.com>
2016-09-13 02:13:51 +0300

    Added 1.st Presentation schedule

---
+++ Async.And.Parallel.Programming/Awaiting.Tasks/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awaiting.Tasks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var task = Task.Run(() => RunFiveMillionIterations());
+
+            RunOneMillionIterations();
+            RunOneMillionIterations();
+            RunOneMillionIterations();
+            RunOneMillionIterations();
+            RunOneMillionIterations();
+
+            Task.WaitAll(task);
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
+        }
+
+        public static void RunOneMillionIterations()
+        {
+            Console.WriteLine("One million iterations - Started!");
+
+            var sum = 0;
+            for (int i = 0; i < 1000000; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("One million iterations - Done!");
+        }
+
+        public static void RunFiveMillionIterations()
+        {
+            Console.WriteLine("Five million iterations - Started!");
+
+            var sum = 0;
+            for (int i = 0; i < 10000000; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("Five million iterations - Done!");
+        }
+    }
+}
