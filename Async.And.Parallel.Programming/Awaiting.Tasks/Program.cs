6c54b6db7a494110f7fb9034472088e574fa649f
InKolev <in.kolev.b@gmail.com>
2016-09-13 01:56:45 -0700

    Added more examples with ThreadPools and TPL

--- Async.And.Parallel.Programming/Awaiting.Tasks/Program.cs
+++ Async.And.Parallel.Programming/Awaiting.Tasks/Program.cs
@@ -3,28 +3,65 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Awaiting.Tasks
 {
     public class Program
     {
+        public static ManualResetEvent resetEvent = new ManualResetEvent(false);
+
         public static void Main(string[] args)
         {
             var stopwatch = Stopwatch.StartNew();
-            var task = Task.Run(() => RunFiveMillionIterations());
 
-            RunOneMillionIterations();
-            RunOneMillionIterations();
-            RunOneMillionIterations();
-            RunOneMillionIterations();
-            RunOneMillionIterations();
+            var t1 = Task.Run(() => RunHundredMillionIterations());
 
-            Task.WaitAll(task);
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                RunHundredMillionIterations();
+                resetEvent.Set();
+            });
+
+            // Wait for queued work item
+            resetEvent.WaitOne();
+
+            // Wait for task
+            Task.WaitAll(t1);
+
             stopwatch.Stop();
-
+            // around 600 ms
             Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
         }
+
+        //public static void Main(string[] args)
+        //{
+        //    var stopwatch = Stopwatch.StartNew();
+
+        //    var t1 = new Thread(RunHundredMillionIterations);
+        //    var t2 = new Thread(RunHundredMillionIterations);
+        //    var t3 = new Thread(RunHundredMillionIterations);
+        //    var t4 = new Thread(RunHundredMillionIterations);
+        //    var t5 = new Thread(RunHundredMillionIterations);
+
+
+        //    t1.Start();
+        //    t2.Start();
+        //    t3.Start();
+        //    t4.Start();
+        //    t5.Start();
+
+        //    t1.Join();
+        //    t2.Join();
+        //    t3.Join();
+        //    t4.Join();
+        //    t5.Join();
+
+        //    // 1289 ms in total
+        //    stopwatch.Stop();
+        //    Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
+        //}
 
         public static void RunOneMillionIterations()
         {
@@ -39,17 +76,17 @@ namespace Awaiting.Tasks
             Console.WriteLine("One million iterations - Done!");
         }
 
-        public static void RunFiveMillionIterations()
+        public static void RunHundredMillionIterations()
         {
-            Console.WriteLine("Five million iterations - Started!");
+            Console.WriteLine("Hundred million iterations - Started!");
 
             var sum = 0;
-            for (int i = 0; i < 10000000; i++)
+            for (int i = 0; i < 100000000; i++)
             {
                 sum += i;
             }
 
-            Console.WriteLine("Five million iterations - Done!");
+            Console.WriteLine("Hundred million iterations - Done!");
         }
     }
 }
