9c9bed115a5c39995a69735329be693566fceaf0
InKolev <in.kolev.b@gmail.com>
2016-09-27 12:43:16 +0300

    Added Race condition demo

---
+++ 1. Multithreaded Programming Fundamentals/Demos/Multithreaded.Programming.Fundamentals/IV. Race.Condition/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IV.Race.Condition
+{
+    public class Startup
+    {
+        private static volatile int counter = 0;
+        private static object counterLock = new object();
+
+        public static void Main(string[] args)
+        {
+            var t1 = new Thread(() => Run(ConsoleColor.Yellow));
+            var t2 = new Thread(() => Run(ConsoleColor.Green));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.ResetColor();
+        }
+
+        public static void Run(ConsoleColor foregroundColor)
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+
+            while (counter < 10)
+            {
+                lock (counterLock)
+                {
+                    if(counter < 10)
+                    {
+                        counter++;
+                        Console.ForegroundColor = foregroundColor;
+                        Console.WriteLine($"[Thread:{threadId}] - {counter}");
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+        }
+    }
+}
