e51bdbecb908dca99ea1f4bf01b148d0f3fedcb0
InKolev <in.kolev.b@gmail.com>
2016-09-28 15:55:15 +0300

    Added Freezing UI Demo

--- 1. Multithreaded Programming Fundamentals/Demos/Multithreaded.Programming.Fundamentals/II. FreezingUI/MainWindow.xaml.cs
+++ 1. Multithreaded Programming Fundamentals/Demos/Multithreaded.Programming.Fundamentals/II. FreezingUI/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace II.FreezingUI
 {
@@ -22,7 +15,50 @@ namespace II.FreezingUI
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+
+        private IEnumerable<Car> ProcessCarsFile(string filePath)
+        {
+            var cars = new List<Car>(600);
+            var lines = File.ReadAllLines(filePath).Skip(2);
+
+            foreach (var line in lines)
+            {
+                cars.Add(Car.Parse(line));
+            }
+
+            return cars;
+        }
+
+        private void BtnFreezeUI_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Why is this not executing?
+                this.btnFreezeUI.Content = "Freezing...";
+                Thread.Sleep(2000);
+                this.btnFreezeUI.Content = "UI has been frozen (Click to freeze again)";
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void BtnShowDetails_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedCar = this.listViewCars.SelectedItem ?? "NO CAR SELECTED";
+            MessageBox.Show(selectedCar.ToString(), "Car details", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnLoadCars_Click(object sender, RoutedEventArgs e)
+        {
+            var carsDataPath = @"..\..\..\Data\cars.csv";
+            var cars = this.ProcessCarsFile(carsDataPath);
+
+            this.listViewCars.ItemsSource = cars;
         }
     }
 }
