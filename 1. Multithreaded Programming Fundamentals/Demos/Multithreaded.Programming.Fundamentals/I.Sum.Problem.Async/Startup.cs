4d1152da8f9b11b5d153c6484f5f6e6f79a77570
InKolev <in.kolev.b@gmail.com>
2016-11-19 20:38:51 +0200

    Changed 1st demo to use Threads instead of Tasks

--- 1. Multithreaded Programming Fundamentals/Demos/Multithreaded.Programming.Fundamentals/I.Sum.Problem.Async/Startup.cs
+++ 1. Multithreaded Programming Fundamentals/Demos/Multithreaded.Programming.Fundamentals/I.Sum.Problem.Async/Startup.cs
@@ -59,55 +59,6 @@ namespace I.Sum.Problem.Async
             Console.WriteLine($"Sum: {totalSum}");
         }
 
-        //public static void Main(string[] args)
-        //{
-        //    var coresCount = Environment.ProcessorCount*2;
-        //    var tasks = new List<Task>(coresCount);
-        //    var arrayProcessors = new List<ArrayProcessor>(coresCount);
-
-        //    // Build array
-        //    var arraySize = 50000000;
-        //    var array = GetArray(arraySize);
-
-        //    var stopwatch = Stopwatch.StartNew();
-
-        //    // Run N Threads to deal with N SMALLER problems
-        //    var elementsPerCore = arraySize / coresCount;
-        //    var elementsLeftOver = arraySize % coresCount;
-        //    for (int i = 0; i < coresCount; i++)
-        //    {
-        //        var startIndex = i * elementsPerCore;
-        //        var elementsToProcessCount = elementsPerCore;
-
-        //        if (i == coresCount - 1)
-        //        {
-        //            elementsToProcessCount += elementsLeftOver;
-        //        }
-
-        //        var arrayProcessor = new ArrayProcessor(array, startIndex, elementsToProcessCount);
-        //        arrayProcessors.Add(arrayProcessor);
-
-        //        var task = Task.Run(() => arrayProcessor.CalculateSum());
-        //        tasks.Add(task);
-        //    }
-
-        //    // Wait for the tasks to finish and calculate the final sum
-        //    Task.WaitAll(tasks.ToArray());
-
-        //    BigInteger totalSum = 0;
-        //    for (int i = 0; i < arrayProcessors.Count; i++)
-        //    {
-        //        totalSum += arrayProcessors[i].CalculatedSum;
-        //    }
-
-        //    stopwatch.Stop();
-
-        //    // Elapsed time: 1900-3300 ms
-        //    // Sum: 1249999975000000
-        //    Console.WriteLine($"Elapsed time: {stopwatch.Elapsed.TotalMilliseconds}");
-        //    Console.WriteLine($"Sum: {totalSum}");
-        //}
-
         public static int[] GetArray(int size)
         {
             var array = new int[size];
